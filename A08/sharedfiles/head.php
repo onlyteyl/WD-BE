19b0fcd03985566255450136e26ff28606024353
teylstack <allenendaya43@gmail.com>
2024-12-06 22:42:22 +0800

    add web icon

--- A08/sharedfiles/head.php
+++ A08/sharedfiles/head.php
@@ -19,4 +19,6 @@
     <link href="assets/style.css" rel="stylesheet">
 
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+
+    <link rel="icon" href="assets/Inside-Out-Logo.webp">
 </head>
